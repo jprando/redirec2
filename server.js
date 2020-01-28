0dbb92d0daeb4206d09beef9224fc65f1325105c
Jeudi Prando <jeudi@adsoft.com.br>
2020-01-28 15:38:51 -0300

    iniciando repositorio

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const {
+  host = 'localhost',
+  port = 3000,
+  redirect2 = 'https://jeudi.dev'
+} = require('./config.json')
+
+http.createServer((req, res) => {
+  res.statusCode = 302
+  res.setHeader('Location', redirect2)
+  res.end()
+}).listen(port, host, () => {
+  console.log(`\rServidor rodando em http://${host}:${port}/`)
+  console.log('\rPara derrubar o servidor, pressione CTRL + C')
+})
